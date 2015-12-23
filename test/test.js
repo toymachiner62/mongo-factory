623996b587644f841f72476aaf00e85799542408
Joshua Jabbour <code@joshuajabbour.com>
2015-12-22 16:01:07 -0800

    Clean up test formatting.

--- test/test.js
+++ test/test.js
@@ -3,41 +3,37 @@ var expect = chai.expect;
 var mongo = require('mongodb');
 
 describe('mongoFactory', function() {
-
   var mongoFactory;
 
   beforeEach(function() {
     mongoFactory = require('../');
   });
 
-  it('can be required without blowing up', function() {
+  it('can be required without blowing up', function(done) {
     var mongoFactory = require('../');
     expect(mongoFactory).to.exist;
+    done();
   });
 
   describe('getConnection()', function() {
-
-    describe('with no arguments passed in', function() {
+    describe('with no parameters', function() {
       it('should fulfill the promise', function(done) {
-        // var mongoFactory = require('../');
-        var con = mongoFactory.getConnection();
-        expect(con).to.be.fulfilled;
+        var conn = mongoFactory.getConnection();
+        expect(conn).to.be.fulfilled;
         done();
       });
 
-      it('should return a rejected promise', function(done) {
-        // var mongoFactory = require('../');
-        var con = mongoFactory.getConnection();
-        expect(con).to.be.rejected;
+      it('should reject the promise', function(done) {
+        var conn = mongoFactory.getConnection();
+        expect(conn).to.be.rejected;
         done();
       });
      });
 
     describe('with a null parameter', function() {
-      it('should return a rejected promise', function(done) {
-        // var mongoFactory = require('../');
-        var con = mongoFactory.getConnection(null);
-        expect(con).to.be.rejected;
+      it('should reject the promise', function(done) {
+        var conn = mongoFactory.getConnection(null);
+        expect(conn).to.be.rejected;
         done();
       });
     });
@@ -45,9 +41,9 @@ describe('mongoFactory', function() {
     describe('with a valid mongo string parameter', function() {
       describe('needing to create the pool the first time', function() {
         it('should return a fulfilled promise', function(done) {
-          var con = mongoFactory.getConnection('mongodb://localhost:27017').then(function(db) {
-            expect(con).to.be.fulfilled;
-            expect(con).to.not.be.rejected;
+          var conn = mongoFactory.getConnection('mongodb://localhost:27017').then(function(db) {
+            expect(conn).to.be.fulfilled;
+            expect(conn).to.not.be.rejected;
             done();
           });
         });
@@ -55,10 +51,10 @@ describe('mongoFactory', function() {
 
       describe('after a pool is already instantiated', function() {
         it('should return a fulfilled promise', function(done) {
-          var con1 = mongoFactory.getConnection('mongodb://localhost:27017').then(function() {
-            var con = mongoFactory.getConnection('mongodb://localhost:27017').then(function() {
-              expect(con).to.be.fulfilled;
-              expect(con).to.not.be.rejected;
+          var conn1 = mongoFactory.getConnection('mongodb://localhost:27017').then(function() {
+            var conn = mongoFactory.getConnection('mongodb://localhost:27017').then(function() {
+              expect(conn).to.be.fulfilled;
+              expect(conn).to.not.be.rejected;
               done();
             });
           });
@@ -78,7 +74,7 @@ describe('mongoFactory', function() {
       expect(oid.toHexString().length).to.equal(24);
     });
 
-    it('should allow you to compare ObjectID\'s', function() {
+    it('should allow you to compare ObjectIDs', function() {
       var oid1 = new mongoFactory.ObjectID;
       var oid2 = new mongoFactory.ObjectID(oid1.id);
       var oid3 = new mongoFactory.ObjectID;
@@ -94,6 +90,5 @@ describe('mongoFactory', function() {
       var oid = new mongoFactory.ObjectID(timestamp);
       expect(oid.getTimestamp().toString()).to.equal(timestampDate.toString());
     });
-
   });
 });
